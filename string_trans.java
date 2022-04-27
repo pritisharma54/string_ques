e7a75e9e71f52f8d221979e84f58d135f2d72291
pritisharma54 <48624579+pritisharma54@users.noreply.github.com>
2022-04-27 23:14:27 +0530

    Create string_trans.java

---
+++ string_trans.java
@@ -0,0 +1,34 @@
+/*
+    Time Complexity - O(N * log(K))
+    Space Complexity - O(K)
+    where K is the window size and N is the length of the input string
+    https://www.codingninjas.com/codestudio/problems/string-transformation_630421?leftPanelTab=0
+*/
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public static String getTransformedString(String str, int k) {
+        StringBuilder answer = new StringBuilder();
+
+        // Build the minHeap
+        PriorityQueue<Character> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < Math.min(k, str.length()); i++) {
+            minHeap.add(str.charAt(i));
+        }
+
+        int indexToBeAdded = k;
+
+        // While all the characters from the string have not been removed,
+        // keep removing the smallest character among the first k characters.
+        while (!minHeap.isEmpty()) {
+            answer.append(minHeap.poll());
+            if (indexToBeAdded < str.length()) {
+                minHeap.add(str.charAt(indexToBeAdded));
+                indexToBeAdded++;
+            }
+        }
+
+        return answer.toString();
+    }
+}
